ef432f929a635f1b501e6b11e6354fb82f79dc2c
Jens.Agersgaard <alex.norgaard@gmail.com>
2018-11-16 11:21:08 +0100

    Created preliminary constructor and started game loop in Controller-class

--- src/main/java/Domain/Controller/Controller.java
+++ src/main/java/Domain/Controller/Controller.java
@@ -1,4 +1,18 @@
 package Domain.Controller;
 
 public class Controller {
+
+    //Constructor
+    public Controller(){
+    startGame();
+    }
+
+    //Starts the game loop
+    public void startGame(){
+        gameLoop();
+    }
+
+    public void gameLoop(){
+
+    }
 }
