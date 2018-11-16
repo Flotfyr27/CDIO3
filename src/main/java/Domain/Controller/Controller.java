9402938ef5a9e49e7d0ca702bedeb7521912fdb7
Jens.Agersgaard <alex.norgaard@gmail.com>
2018-11-16 12:00:57 +0100

    Added do while loop to Controller class.

--- src/main/java/Domain/Controller/Controller.java
+++ src/main/java/Domain/Controller/Controller.java
@@ -11,6 +11,11 @@ public class Controller {
     // Game loop
     public void gameLoop(){
 
+    //Loop that runs the game in correct order
+        do{
+
+        }while(/*a player has not won*/);
+
     }
 
 
