973817e47e5a4a445abccf71bd0e0cd25f60cd99
Fredrik <Fredrik200619@hotmail.com>
2018-11-19 22:44:53 +0100

    still working on it

--- src/main/java/UI/GUI/GUI.java
+++ src/main/java/UI/GUI/GUI.java
@@ -2,19 +2,18 @@
  * All GUI related classes are kept here.
  */
 package UI.GUI;
+
+import Domain.GameElements.Fields.Field;
 import gui_fields.GUI_Field;
 import gui_fields.GUI_Street;
-import org.graalvm.compiler.core.common.Fields;
 
 import javax.swing.*;
 
-//import desktop_fields.*;
-
-public class GUI<field> {
+public class GUI {
 
     private int playerTotal;
     private int fieldNum = 24;
-
+    private Field[] bfields;
     public GUI(int numberOfPlayers) {
         JFrame jFrame = new JFrame("message");
         jFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
@@ -33,21 +32,19 @@ public class GUI<field> {
         testStreet.getRent();
         fields[1] = testStreet;
     }
-    public void initfields    { //vrøl. det her giver ingen mening i sproget.
-    }
 
-        //for loop, for hvert field der er. Den skal sætte det ind i et array
+        //for loop, for hvert field der er. Der skal sætte det ind i et array
         //lav et nyt array og return det array
-
-public int GUI_Field[] //dette er også skrevet forkert en metode skrives: public int GUI_Field[] <metodenavn>(){}
-
-    {
+        //dette er også skrevet forkert en metode skrives: public int GUI_Field[] <metodenavn>(){}
+public int GUI_Field[] afield()  {
+       GUI_Field = new int[bfields][];
         for (int i = 0; i < GUI_Field.length; i++){
-            GUI_Field[i]= new int initfields[i];//her bruger du vrøvl fra tidligere
+            GUI_Field[i] = new bfields[i];
+            return bfields[]
+        }
 
-            return field[i];
 
-    }
+
 
 
     }
