400db6605b9cfc68cf87d91d6308e9b905774d8e
Jens.Agersgaard <alex.norgaard@gmail.com>
2018-11-19 16:01:36 +0100

    Modified movePlayer method in Board class, and added to controller

--- src/main/java/Domain/Controller/Controller.java
+++ src/main/java/Domain/Controller/Controller.java
@@ -1,14 +1,19 @@
 package Domain.Controller;
 
 
-public class Controller {
-    int NumberOfPlayers;
-    Field[] Fields;
+import Domain.GameElements.Board;
+import Domain.GameElements.Entities.Die;
 
+public class Controller {
+
+    private Die die;
+    private int pIndex;
+    private Board board;
     //Constructor - starts game loop
-    public Controller(int NumberOfPlayers){
-        this NumberOfPlayers = NumberOfPlayers;
-        Board.movePlaver(P,roll);
+    public Controller(Board board){
+        die = new Die();
+        this.board = board;
+        pIndex = 0;
         gameLoop();
     }
 
@@ -18,9 +23,15 @@ public class Controller {
     //Loop that runs the game in correct order
         do{
 
+            Board.movePlayer(board.getPlayers()[pIndex],die.Roll());
+
+            nextPlayer();
         }while(/*a player has not won*/);
 
     }
 
+    private void nextPlayer(){
+        pIndex = ++pIndex%board.getPlayers().length;
+    }
 
 }
--- src/main/java/Domain/GameElements/Board.java
+++ src/main/java/Domain/GameElements/Board.java
@@ -6,15 +6,21 @@ import Domain.GameElements.Fields.Field;
 public class Board {
     private Player[] players;
     private Field[] fields;
-    private Die die;
+
 
     public Board() {
         players = new Player[2];
         fields = new Field[24];
-        die = new Die();
+
     }
 
     public void movePlayer(Player p, int dist){
-        p.setPos(dist);
+        int currentPos = p.getPos();
+        currentPos = (currentPos + dist)%fields.length;
+        p.setPos(currentPos);
+    }
+
+    public Player[] getPlayers() {
+        return players;
     }
 }
