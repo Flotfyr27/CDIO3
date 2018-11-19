eb2ed3c9005932b6500e20df5b37fd080c715dcf
Jens.Agersgaard <alex.norgaard@gmail.com>
2018-11-19 13:57:26 +0100

    added die to board class

--- src/main/java/Domain/GameElements/Board.java
+++ src/main/java/Domain/GameElements/Board.java
@@ -1,15 +1,17 @@
 package Domain.GameElements;
 
-import Domain.GameElements.Entities.Player;
+import Domain.GameElements.Entities.*;
 import Domain.GameElements.Fields.Field;
 
 public class Board {
     private Player[] players;
     private Field[] fields;
+    private Die die;
 
     public Board() {
         players = new Player[2];
         fields = new Field[24];
+        die = new Die();
     }
 
     public void movePlayer(Player p, int dist){
