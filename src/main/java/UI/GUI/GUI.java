a3c5c9a0f5177a7b2803346478083f591d68e3f7
Fredrik <polostyre@hotmail.com>
2018-11-20 11:28:00 +0100

    Working on the contructor for GUI_Fields

--- src/main/java/UI/GUI/GUI.java
+++ src/main/java/UI/GUI/GUI.java
@@ -4,6 +4,7 @@
 package UI.GUI;
 
 import Domain.GameElements.Fields.Field;
+import gui_fields.GUI_Empty;
 import gui_fields.GUI_Field;
 import gui_fields.GUI_Street;
 
@@ -12,9 +13,9 @@ import javax.swing.*;
 public class GUI {
 
     private int playerTotal;
-    private int fieldNum = 24;
-    private Field[] bfields;
-    public GUI(int numberOfPlayers) {
+    GUI_Field[] gui_fields;
+
+    public GUI(int numberOfPlayers, Field[] fields) {
         JFrame jFrame = new JFrame("message");
         jFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         jFrame.setVisible(true);
@@ -22,27 +23,28 @@ public class GUI {
         playerTotal = numberOfPlayers;
 
 
-                // number of fields
-                GUI_Field[] fields = new GUI_Field[fieldNum];
+        // number of fields
+        initGuiFields(fields);
 
 
         GUI_Street MatadorStreet = new GUI_Street();
-        GUI_Street testStreet= new GUI_Street();
+        GUI_Street testStreet = new GUI_Street();
         testStreet.getTitle();
         testStreet.getRent();
-        fields[1] = testStreet;
     }
 
         //for loop, for hvert field der er. Der skal sætte det ind i et array
         //lav et nyt array og return det array
         //dette er også skrevet forkert en metode skrives: public int GUI_Field[] <metodenavn>(){}
-public int GUI_Field[] afield()  {
-       GUI_Field = new int[bfields][];
-        for (int i = 0; i < GUI_Field.length; i++){
-            GUI_Field[i] = new bfields[i];
-            return bfields[]
+public GUI_Field[] initGuiFields(Field[] fields)  {
+
+       gui_fields = new GUI_Field[fields.length];
+
+       for (int i = 0; i < fields.length; i++){
+           GUI_Empty
         }
 
+        return
 
 
 
