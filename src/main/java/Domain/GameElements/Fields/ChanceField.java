f010e0442b434e6bde2a76c4476b92b7104ca67f
LGramme <42938246+LGramme@users.noreply.github.com>
2018-11-25 22:02:12 +0100

    Added ChanceField.java and constructor as subclass of Field.java

---
+++ src/main/java/Domain/GameElements/Fields/ChanceField.java
@@ -0,0 +1,8 @@
+package Domain.GameElements.Fields;
+
+public class ChanceField extends Field {
+
+    public ChanceField(String name, String description){
+        super(name, description);
+    }
+}
