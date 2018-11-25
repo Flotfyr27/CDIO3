2b410158a18d702fd6182d29a5e58dbbe1f51879
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2018-11-25 18:03:37 +0100

    Emptied GuiHandler.java

--- src/main/java/UI/GUI/GuiHandler.java
+++ src/main/java/UI/GUI/GuiHandler.java
@@ -12,26 +12,6 @@ import java.awt.*;
 
 public class GuiHandler {
 
-   private GUI_Field[] fieldObj;
-   private GUI gui;
-
-    /**
-     * This method takes an array of type Field and fills an object of type GUI_Fields[] with GUI_Street elements.
-     * @param fields This parameter is given when the method is called an consists of all the fields necessary to describe the gameboard.
-     */
-    public void initGuiFields(Field[] fields) {
-        fieldObj = new GUI_Field[fields.length];
-        for(int i = 0; i < fields.length; i++){
-            //fieldObj[i] = (new GUI_Street(fields[i].getTitle(), fields[i].getSubtext(), fields[i].getDescription(), fields[i].getRent(), Color.lightGray, null));
-            fieldObj[i] = (new GUI_Street("Title", "Subtext", "Description", "200", Color.lightGray, null));
-        }
-    }
-
-
-    public GuiHandler(int numberOfPlayers, Field[] fields) {
-        initGuiFields(fields);
-        gui = new GUI(fieldObj);
-    }
 }
 
 
