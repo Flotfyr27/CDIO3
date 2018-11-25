728fe1928822c1a6d7819d2d017618ec73c8f5ce
LGramme <42938246+LGramme@users.noreply.github.com>
2018-11-25 22:12:22 +0100

    Added javadoc comment for ChanceField.java constructor.

--- src/main/java/Domain/GameElements/Fields/ChanceField.java
+++ src/main/java/Domain/GameElements/Fields/ChanceField.java
@@ -4,8 +4,23 @@ import Domain.GameElements.Entities.ChanceCard;
 
 public class ChanceField extends Field {
     private ChanceCard[] chanceCards;
+
+    /**
+     * Constructor for ChanceField, gathers name, description and chancecards to create the field.
+     * @param name Name of the field.
+     * @param description The fields description.
+     * @param chanceCards An array of chance cards supplied by the ChanceCard class.
+     */
     public ChanceField(String name, String description, ChanceCard[] chanceCards){
         super(name, description);
         this.chanceCards = chanceCards;
+    }
+
+    /**
+     * Performs an action when a player lands on the field.
+     */
+    @Override
+    public void landOnAction() {
+        super.landOnAction();
     }
 }
