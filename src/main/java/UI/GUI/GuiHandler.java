9c189f770d96925e83e2db0e7853b6222ffcb704
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2018-11-25 17:29:47 +0100

    Added filler data for GuiHandler whilst field classes are not implemented

--- src/main/java/UI/GUI/GuiHandler.java
+++ src/main/java/UI/GUI/GuiHandler.java
@@ -20,15 +20,17 @@ public class GuiHandler {
      * @param fields This parameter is given when the method is called an consists of all the fields necessary to describe the gameboard.
      */
     public void initGuiFields(Field[] fields) {
+        fieldObj = new GUI_Field[fields.length];
         for(int i = 0; i < fields.length; i++){
-            fieldObj[i] = (new GUI_Street(fields[i].getTitle(), fields[i].getSubtext(), fields[i].getDescription(), fields[i].getRent()));
+            //fieldObj[i] = (new GUI_Street(fields[i].getTitle(), fields[i].getSubtext(), fields[i].getDescription(), fields[i].getRent(), Color.lightGray, null));
+            fieldObj[i] = (new GUI_Street("Title", "Subtext", "Description", "200", Color.lightGray, null));
         }
     }
 
 
     public GuiHandler(int numberOfPlayers, Field[] fields) {
         initGuiFields(fields);
-        gui = new GUI(fieldObj, Color.WHITE);
+        gui = new GUI(fieldObj);
     }
 }
 
