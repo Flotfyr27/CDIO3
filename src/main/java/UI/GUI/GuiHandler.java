706f20a04cd7a9a845d1ecb3cae7a77fad9d2e76
LGramme <42938246+LGramme@users.noreply.github.com>
2018-11-26 00:29:01 +0100

    Started to use methods to import parameters for the GuiHandler.java, a lot of work is needed.

--- src/main/java/UI/GUI/GuiHandler.java
+++ src/main/java/UI/GUI/GuiHandler.java
@@ -14,9 +14,9 @@ public class GuiHandler {
     GUI gui;
     GUI_Field[] gui_field = new GUI_Field[24];
     //TODO : Change constructor so that fields are read from a parameter and not hardcoded.
-    public GuiHandler(){
+    public GuiHandler(Field[] fields){
         for(int i = 0; i < gui_field.length; i++){
-            gui_field[i] = (new GUI_Street("Title", "Subtext", "Description", "200", Color.lightGray, null));
+            gui_field[i] = (new GUI_Street(fields[i].getName(), "", fields[i].getDescription(), "200", Color.lightGray, null));
         }
         gui = new GUI(gui_field, Color.GREEN);
     }
