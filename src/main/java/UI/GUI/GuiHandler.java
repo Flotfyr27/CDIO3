f92067ba6d995696b91a87b8bfa5828aa61cd03d
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2018-11-25 17:20:51 +0100

    Added contents to initGuiFields-method in GuiHandler.java

--- src/main/java/UI/GUI/GuiHandler.java
+++ src/main/java/UI/GUI/GuiHandler.java
@@ -15,9 +15,16 @@ public class GuiHandler {
    private GUI_Field[] fieldObj;
    private GUI gui;
 
+    /**
+     * This method takes an array of type Field and fills an object of type GUI_Fields[] with GUI_Street elements.
+     * @param fields This parameter is given when the method is called an consists of all the fields necessary to describe the gameboard.
+     */
     public void initGuiFields(Field[] fields) {
-
+        for(int i = 0; i < fields.length; i++){
+            fieldObj[i] = (new GUI_Street(fields[i].getTitle(), fields[i].getSubtext(), fields[i].getDescription(), fields[i].getRent()));
+        }
     }
+
 
     public GuiHandler(int numberOfPlayers, Field[] fields) {
         initGuiFields(fields);
