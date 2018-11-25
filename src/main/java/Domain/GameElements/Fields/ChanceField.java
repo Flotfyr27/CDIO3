1667788236a4c45de03b0efff1dbb5c11b1303dd
LGramme <42938246+LGramme@users.noreply.github.com>
2018-11-25 22:05:56 +0100

    In ChanceField.java added variable chanceCards of type ChanceCard[] to class and constructor.

--- src/main/java/Domain/GameElements/Fields/ChanceField.java
+++ src/main/java/Domain/GameElements/Fields/ChanceField.java
@@ -1,8 +1,11 @@
 package Domain.GameElements.Fields;
 
-public class ChanceField extends Field {
+import Domain.GameElements.Entities.ChanceCard;
 
-    public ChanceField(String name, String description){
+public class ChanceField extends Field {
+    private ChanceCard[] chanceCards;
+    public ChanceField(String name, String description, ChanceCard[] chanceCards){
         super(name, description);
+        this.chanceCards = chanceCards;
     }
 }
