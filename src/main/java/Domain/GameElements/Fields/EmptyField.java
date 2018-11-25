5e72b1babff739ffb6d8531b09359fca87c68feb
LGramme <42938246+LGramme@users.noreply.github.com>
2018-11-25 22:31:30 +0100

    Added javadoc comment to the constructor for EmptyField.java

--- src/main/java/Domain/GameElements/Fields/EmptyField.java
+++ src/main/java/Domain/GameElements/Fields/EmptyField.java
@@ -1,7 +1,11 @@
 package Domain.GameElements.Fields;
 
 public class EmptyField extends Field {
-
+    /**
+     * EmptyField is a subclass of field meant to fill the role of START, prison and other misc fields.
+     * @param name Name of the field.
+     * @param description The fields description.
+     */
     public EmptyField(String name, String description){
         super(name, description);
     }
