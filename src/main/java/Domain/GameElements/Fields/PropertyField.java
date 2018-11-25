1daa8f44ac28745e15c158d3137a68caf4d113d1
LGramme <42938246+LGramme@users.noreply.github.com>
2018-11-25 21:59:26 +0100

    Added javadoc comment for constructor belonging to PropertyField.java. Added placeholder landOnAction method.

--- src/main/java/Domain/GameElements/Fields/PropertyField.java
+++ src/main/java/Domain/GameElements/Fields/PropertyField.java
@@ -6,10 +6,24 @@ public class PropertyField extends Field {
 
     private int rent, price;
     private Player owner;
+
+    /**
+     * Constructor for PropertyField
+     * @param name Name of the field
+     * @param description Description of the field
+     * @param rent Rent to pay for when another player lands on the field
+     * @param price Price to purchase the property
+     * @param owner Current owner of the property
+     */
     public PropertyField(String name, String description, int rent, int price, Player owner){
         super(name, description);
         this.rent = rent;
         this.price = price;
         this.owner = owner;
+    }
+
+    @Override
+    public void landOnAction() {
+        super.landOnAction();
     }
 }
