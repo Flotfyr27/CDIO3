beaea1ad30f3475277a46c173e169dec8ddff6ae
LGramme <42938246+LGramme@users.noreply.github.com>
2018-11-26 20:11:56 +0100

    Changed constructor for GuiHandler to use subtext and not description.

--- src/main/java/UI/GUI/GuiHandler.java
+++ src/main/java/UI/GUI/GuiHandler.java
@@ -18,14 +18,14 @@ public class GuiHandler {
     public GuiHandler(Field[] fields){
         for(int i = 0; i < gui_field.length; i++){
             if(fields[i].getClass().equals(EmptyField.class) && i == 0){
-                gui_field[i] = (new GUI_Start(fields[i].getName(), "", fields[i].getSubtext(), Color.lightGray, null));
+                gui_field[i] = (new GUI_Start(fields[i].getName(), fields[i].getSubtext(), "", fields[i].getBgColour(), null));
             }else if(fields[i].getClass().equals(PropertyField.class)){
                 PropertyField propertyField = (PropertyField) fields[i];
-                gui_field[i] = (new GUI_Street(fields[i].getName(), "", fields[i].getSubtext(), Integer.toString(propertyField.getRent()), Color.lightGray, null));
+                gui_field[i] = (new GUI_Street(fields[i].getName(), fields[i].getSubtext(), "", Integer.toString(propertyField.getPrice()), fields[i].getBgColour(), null));
             }else if(fields[i].getClass().equals(EmptyField.class)){
-                gui_field[i] = (new GUI_Empty(Color.lightGray, null, fields[i].getName(), "", fields[i].getSubtext()));
+                gui_field[i] = (new GUI_Empty(fields[i].getBgColour(), null, fields[i].getName(), fields[i].getSubtext(), ""));
             }else if(fields[i].getClass().equals(ChanceField.class)){
-                gui_field[i] = (new GUI_Chance(fields[i].getName(), "", fields[i].getSubtext(), Color.lightGray, null));
+                gui_field[i] = (new GUI_Chance(fields[i].getName(), fields[i].getSubtext(), "", fields[i].getBgColour(), null));
             }
         }
         gui = new GUI(gui_field, Color.GREEN);
