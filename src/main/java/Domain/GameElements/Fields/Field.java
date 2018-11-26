7e73fe6a1b586bfb35ba16a89f29a019e2d14e84
LGramme <42938246+LGramme@users.noreply.github.com>
2018-11-26 18:42:13 +0100

    Added Color variable to constructor for Field.java

--- src/main/java/Domain/GameElements/Fields/Field.java
+++ src/main/java/Domain/GameElements/Fields/Field.java
@@ -1,11 +1,15 @@
 package Domain.GameElements.Fields;
 
+import java.awt.*;
+
 public abstract class Field {
      private String name, description;
+     private Color bgColor
 
-     public Field(String name, String description){
+     public Field(String name, String description, Color bgColor){
          this.name = name;
          this.description = description;
+         this.bgColor = bgColor;
      }
 
      public String getName(){
@@ -14,5 +18,6 @@ public abstract class Field {
      public String getDescription(){
          return description;
      }
+     public Color getBgColor(){ return bgColor;}
      public void landOnAction(){}
 }
