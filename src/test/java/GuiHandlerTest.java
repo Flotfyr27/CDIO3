250c0e073c931a59066d6ddf92431ad33dfb4ad3
Mads Møller Jensen <mads1606@gmail.com>
2018-11-26 21:54:21 +0100

    commented out some unfinished code

--- src/test/java/GuiHandlerTest.java
+++ src/test/java/GuiHandlerTest.java
@@ -3,6 +3,6 @@ import UI.GUI.GuiHandler;
 public class GuiHandlerTest {
 
     public static void main(String[] args) {
-       GuiHandler guiHandler = new GuiHandler();
+       //GuiHandler guiHandler = new GuiHandler();
     }
 }
