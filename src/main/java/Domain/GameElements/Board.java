f30ae44873e2b4d1a9883b3d0a9a9877b9803016
LGramme <42938246+LGramme@users.noreply.github.com>
2018-11-26 19:56:57 +0100

    Changed fields to values corresponding with supplied game board in Board.java

--- src/main/java/Domain/GameElements/Board.java
+++ src/main/java/Domain/GameElements/Board.java
@@ -42,29 +42,29 @@ public class Board {
      * Method generates a bunch of fields in the shape of the provided monopoly game board
      */
     private void setFields(){
-        fields[0] = (new EmptyField("Start", "Land on or pass this field and receive cash"));
-        fields[1] = (new PropertyField("Vej1", "Dette er en vej", 200, 2000));
-        fields[2] = (new PropertyField("Vej2", "Dette er en vej", 150, 1500));
-        fields[3] = (new ChanceField("Chance!", "You only get one shot do not miss your chance to blow!",chanceField.getChanceCards()));
-        fields[4] = (new PropertyField("Vej3", "Dette er sjovt nok også en vej", 200, 2000));
-        fields[5] = (new PropertyField("Vej4", "Bare en vej", 200, 2000));
-        fields[6] = (new EmptyField("På besøg i fængsel", "Du er bare på besøg"));
-        fields[7] = (new PropertyField("Vej5", "Endnu en vej", 250, 2500));
-        fields[8] = (new PropertyField("Vej6", "Endnu en vej", 250, 2500));
-        fields[9] = (new ChanceField("Chance!", "This opportunity only comes once in a life time yo!", chanceField.getChanceCards()));
-        fields[10] = (new PropertyField("Vej7", "En vej", 150, 1500));
-        fields[11] = (new PropertyField("Vej8", "Vejen", 150, 1500));
-        fields[12] = (new EmptyField("Free parking", "Det gratis"));
-        fields[13] = (new PropertyField("Vej9", "Endnu en", 100, 1000));
-        fields[14] = (new PropertyField("Vej10", "Fin vej", 100, 1000));
-        fields[15] = (new ChanceField("Chance!", "A second chance!", chanceField.getChanceCards()));
-        fields[16] = (new PropertyField("Vej11", "Lidt bedre end andre gode veje", 300, 3000));
-        fields[17] = (new PropertyField("Vej12", "Også en del af klubben", 300, 3000));
-        fields[18] = (new EmptyField("Fængsel ;)", "Good shit det ikke er implementeret"));
-        fields[19] = (new PropertyField("Vej13", "Se mine dårlige donuts på din villavej", 200, 2000));
-        fields[20] = (new PropertyField("Vej14", "Vi har været der!", 200, 2000));
-        fields[21] = (new ChanceField("Chance!", "Keep 'em coming", chanceField.getChanceCards()));
-        fields[22] = (new PropertyField("Vej15", "En flot vej", 400, 4000));
-        fields[23] = (new PropertyField("Vej16", "Den bedste vej", 425, 4250));
+        fields[0] = (new EmptyField("START", "Modtage $2, når du passerer", Color.lightGray));
+        fields[1] = (new PropertyField("BURGERBAREN", "$1", BROWN, 1));
+        fields[2] = (new PropertyField("PIZZARIAET", "$1", BROWN, 1));
+        fields[3] = (new ChanceField("CHANCE", "",Color.lightGray, chanceCardArray));
+        fields[4] = (new PropertyField("SLIKBUTIKKEN", "$1", Color.cyan, 1));
+        fields[5] = (new PropertyField("ISKIOSKEN", "$1", Color.cyan, 1));
+        fields[6] = (new EmptyField("PÅ BESØG", "", Color.lightGray));
+        fields[7] = (new PropertyField("MUSEET", "$2", Color.magenta, 2));
+        fields[8] = (new PropertyField("BIBLIOTEKET", "$2", Color.magenta, 2));
+        fields[9] = (new ChanceField("CHANCE", "", Color.lightGray, chanceCardArray));
+        fields[10] = (new PropertyField("SKATERPARKEN", "$2", HOT_PINK, 2));
+        fields[11] = (new PropertyField("SWIMMINGPOOLEN", "$2", HOT_PINK, 2));
+        fields[12] = (new EmptyField("GRATIS PARKERING", "", Color.lightGray));
+        fields[13] = (new PropertyField("SPILLEHALLEN", "$3", Color.RED, 3));
+        fields[14] = (new PropertyField("BIOGRAFEN", "$3", Color.RED, 3));
+        fields[15] = (new ChanceField("CHANCE", "", Color.lightGray, chanceCardArray));
+        fields[16] = (new PropertyField("LEGETØJSBUTIKKEN", "$3", Color.YELLOW, 3));
+        fields[17] = (new PropertyField("DYREHANDLEN", "$3", Color.YELLOW, 3));
+        fields[18] = (new EmptyField("GÅ I FÆNGSEL", "", Color.lightGray));
+        fields[19] = (new PropertyField("BOWLINGHALLEN", "$4", Color.GREEN, 4));
+        fields[20] = (new PropertyField("ZOO", "$4", Color.GREEN, 4));
+        fields[21] = (new ChanceField("CHANCE", "", Color.lightGray, chanceCardArray));
+        fields[22] = (new PropertyField("VANDLANDET", "$5", Color.BLUE, 5));
+        fields[23] = (new PropertyField("STRANDPROMENADEN", "$5", Color.BLUE, 5));
     }
 }
