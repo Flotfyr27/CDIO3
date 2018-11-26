9c8a8df4deca57b4c59289ef1f648243050edd72
LGramme <42938246+LGramme@users.noreply.github.com>
2018-11-26 23:20:22 +0100

    Commented and uncommented things in GuiHandler.java and Board.java. Also created launch conditions in GuiHandlerTest.java. Gui can now successfully launch from test class.

--- src/test/java/GuiHandlerTest.java
+++ src/test/java/GuiHandlerTest.java
@@ -1,8 +1,10 @@
+import Domain.GameElements.Board;
 import UI.GUI.GuiHandler;
 
 public class GuiHandlerTest {
 
     public static void main(String[] args) {
-       //GuiHandler guiHandler = new GuiHandler();
+        Board board = new Board(2);
+       GuiHandler guiHandler = new GuiHandler(board.getFields());
     }
 }
