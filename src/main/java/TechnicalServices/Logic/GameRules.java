058a794b01e136831a33319b4826955b17c6cc73
LGramme <42938246+LGramme@users.noreply.github.com>
2018-11-28 00:55:23 +0100

    Fixed spelling and added capsulation for GameRules.java

--- src/main/java/TechnicalServices/Logic/GameRules.java
+++ src/main/java/TechnicalServices/Logic/GameRules.java
@@ -3,17 +3,19 @@ package TechnicalServices.Logic;
 import Domain.GameElements.Entities.Player;
 
 /**
- * A class checking whether some rule comes into effect. mainly win and loose conditions
+ * A class checking whether some rule comes into effect. mainly win and lose conditions
  */
 public class GameRules {
     /**
-     * This method checks if the player has lost or not
+     * This method checks if a player has lost or not
      * @param player The player object being examined
      * @return True if the player has lost
      */
     public boolean hasLost(Player player){
-        if (player.getAccount().getScore() < 0)
+        if (player.getAccount().getScore() < 0) {
             return true;
-        return false;
+        }else {
+            return false;
+        }
     }
 }
