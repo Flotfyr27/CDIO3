3857bb5bb5f901a8eb55ac729ecf1e18eff782f8
LGramme <42938246+LGramme@users.noreply.github.com>
2018-11-27 21:46:00 +0100

    Emptied GuiHandlerTest.java for redundant code

--- src/test/java/GuiHandlerTest.java
+++ src/test/java/GuiHandlerTest.java
@@ -1,11 +1,3 @@
-import Domain.GameElements.Board;
-import UI.GUI.GuiHandler;
-
 public class GuiHandlerTest {
 
-    public static void main(String[] args) {
-        Board board = new Board(2);
-       GuiHandler guiHandler = new GuiHandler(board.getFields());
-       guiHandler.addButton();
-    }
 }
